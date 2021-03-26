783dd757b17e2bc296cff804a8373e4125eb3019
zhuzonbir <phnix11.11@gmail.com>
2021-03-26 21:06:57 +0600

    izvinyayus

---
+++ 6-2.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+void main() {
+  // stdout.write("chislo  ");                   //Выводит введенные числа в обратном порядке
+  // var x = (stdin.readLineSync()!.split(""));
+  // var y = [];
+  // for (int i = x.length - 1; i >= 0; i--) {В
+  //   y.add(x[i]);
+  // }
+  // print(y.join(""));
+
+  // var a = [1, 3, 5, 7, 9];
+
+//   int i = a.length-1;
+//   while(i >= 0){
+// print(a[i]);
+// --i;
+
+//   List x = [450, 45, 5, 550, 150, 140, 250,550];
+//   var z = 1000;
+//   var y = [];
+
+//   for (var i = 0; i < x.length; i++) {
+//     for (var i2 = i; i2 < x.length; i2++) {
+//       if (x[i] + x[i2] == z && x.indexOf(x[i]) != x.indexOf([i2])) {
+//       y.add(i);
+//       y.add(i2);
+//     }
+//   }
+// }
+// print(y);
+
+// List l1 = [1,2,4];
+// List l2 = [1,3,4];
+// var x = [];
+// var z = l1+l2;
+// print(z);
+// int k= z.length;
+// for(var i = 0; i < k; i++){
+//    var h = z.cast<num>().reduce(min);
+//    x.add(h);
+//    z.remove(h);
+//    print(h);
+// }
+// print(x);
+
+// }
+}
